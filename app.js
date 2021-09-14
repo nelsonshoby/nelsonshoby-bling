4b43475bb8c367bc26c038fc19e56ccc57b976a0
nelson shoby <nelsonshoby009@gmail.com>
2021-09-14 23:16:33 +0530

    Built all functionalities

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const container = document.createElement("div");
-document.body.appendChild(container);
+
+/*header*/
 
 const header = document.createElement("div");
-container.appendChild(header);
+root.appendChild(header);
 
 const title = document.createElement("h1");
 title.innerText += "BLING"
@@ -10,7 +10,7 @@ title.style.cssText = "padding-top : 1em;color :#2d2d2d;text-shadow: 2px 2px #f3
 header.appendChild(title);
 
 const main = document.createElement("div");
-container.appendChild(main)
+root.appendChild(main)
 main.style.cssText = "color:black;display:flex; flex-direction: row;justify-content:space-around;margin-top:100px"
 
 
@@ -23,17 +23,20 @@ left_main.style.cssText = "width:50%,margin-top:5%"
 const text_select_radio = document.createElement("div");
 text_select_radio.style.cssText += "display : flex; justify-content:space-between; width:60vh "
 
+/*Sizzle your life */
 const left_main_first = document.createElement("h3");
 left_main_first.innerText = "Sizzle your life"
 left_main.appendChild(left_main_first);
 left_main_first.style.cssText = "margin : 10px 0 10px 0"
 
+/*Make a card */
 
 const left_main_second = document.createElement("h1");
 left_main_second.innerText = "Make a card"
 left_main.appendChild(left_main_second);
 left_main_second.style.cssText = "margin : 20px 0 10px 0;font-weight: bold;font-size: 30px;"
 
+/*text box*/
 
 const left_main_third = document.createElement("input");
 left_main_third.placeholder = "...Title"
@@ -47,6 +50,7 @@ left_main_third.addEventListener('input',updateValue);
 function updateValue(e) {
     right_main_first.textContent = e.target.value;
 }
+/* color */
 
 let color_list = ["Snow","DodgerBlue","LightSalmon","LightCoral","LightPink","OrangeRed","Tomato","DarkOrchid","SpringGreen","Thistle"]
 const left_main_fourth = document.createElement("select");
@@ -64,8 +68,10 @@ color_list.forEach((item)=>{
     left_main_fourth.appendChild(options)
     
 })
+
 left_main_fourth.addEventListener('change',changeColor)
 function changeColor(e){
+    right_main.style.backgroundColor = e.target.value;
     document.body.style.backgroundColor = e.target.value;
 }
 
@@ -90,6 +96,9 @@ function changeBackGroundImage(e){
     right_main.style.backgroundImage = `url('assets/images/blob.png')`
     right_main.style.backgroundSize = "100%"
     right_main.style.backgroundRepeat = "no-repeat"  
+    right_main.style.backgroundPosition = "0px 50px"
+
+    
 }
 /*radio button(blob) label*/
 
@@ -108,6 +117,7 @@ radio_two.addEventListener('change',removeBackGroundImage)
 
 function removeBackGroundImage(e){
     right_main.style.background = 'none';
+
 }
 
 /*radio button(none) label*/
@@ -123,6 +133,9 @@ main.appendChild(right_main);
 
 /* paragraph above the image*/
 const right_main_first = document.createElement("p");
+right_main_first.style.fontSize = "50px";
+right_main_first.style.fontWeight = "bold"
+right_main_first.style.marginBottom = "100px"
 right_main.appendChild(right_main_first);
 
 
@@ -155,11 +168,33 @@ function showPicture(e) {
 let cam_image = document.createElement("button");
 cam_image.style.backgroundImage = `url('assets/images/camera.png')`
 cam_image.style.padding ="15px 15px";
+cam_image.style.boxShadow = "5px 3px 3px black";
 cam_image.style.backgroundSize = "50%"
 cam_image.style.backgroundPosition = "center";
-cam_image.style.backgroundRepeat = "no-repeat" 
+cam_image.style.backgroundRepeat = "no-repeat" ;
 
-
+cam_image.addEventListener('click',cloneDiv);
+function cloneDiv(e){
+    const clone = right_main.cloneNode(true);
+    clone.style.border = "1px solid black";
+    clone.style.boxShadow = "5px 3px 3px black";
+    
+    clone.style.margin = "50px"
+    clone.style.position = "relative";
+    /*close button */
+    const close_btn = document.createElement("img");
+    close_btn.src = 'assets/images/icon_delete.png'
+    close_btn.style.position = "absolute"
+    close_btn.style.height = "50px";
+    close_btn.style.top = "-15px"
+    close_btn.style.right = "-15px"
+    close_btn.addEventListener('click',()=>clone.remove())
+    /*close_btn.style.top = "-20px"
+    close_btn.style.right = "-20px"*/
+    clone.appendChild(close_btn)
+    card_div.appendChild(clone)
+    
+}
 /*image button */
 
 btnList.forEach((items,index)=>{
@@ -167,11 +202,30 @@ const button = document.createElement("button");
 button.className += "button";
 button.innerText=items;
 button.value = imageList[index];
+button.style.borderRadius = "0.5em";
 button.style.cssText ="color: black;font-size: 16px; margin-top:20px;margin-right:16px;background-color:inherit"
+button.style.boxShadow = "5px 3px 3px black";
 button.addEventListener('click',showPicture);
 left_main.appendChild(button)
 })
 right_main.appendChild(image);
+
+/* Card div */
+const card_div = document.createElement("div");
+card_div.style.cssText = " display:flex; justify-content : space-around;flex-wrap :wrap;"
+root.appendChild(card_div);
+
+/*footer*/
+let footer = document.createElement("footer");
+
+footer.innerText = "🖤 Credits : Polaroid TV Traitor Fall Guy Radio";
+footer.style.position = "relative";
+footer.style.paddingTop = "15%";
+footer.style.bottom = "1em";
+footer.style.width = "100%";
+footer.style.textAlign = "center";
+root.append(footer);
+
 
 
 left_main.appendChild(text_select_radio)
